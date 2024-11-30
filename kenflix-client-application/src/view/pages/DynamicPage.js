6bb20e6463dda892807424a82a0edaad9edc635e
Evans <murungaevans84@gmail.com>
2024-11-30 16:41:03 +0300

    Initial commit

---
+++ kenflix-client-application/src/view/pages/DynamicPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DynamicPage = ({ name }) => {
+    return (
+        <div>
+            <h1>Hello from {name} page!</h1>
+        </div>
+    );
+};
+
+export default DynamicPage;
